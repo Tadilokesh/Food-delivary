0e1d59e320704deeddf7c77744a45961f4b3106e
Tadi <tadilokesh8@gmail.com>
2024-11-27 12:22:40 +0530

    To-do-list

---
+++ src/components/TodoWrapper.js
@@ -0,0 +1,72 @@
+import React, { useState, useEffect } from "react";
+import { Todo } from "./Todo";
+import { TodoForm } from "./TodoForm";
+import { v4 as uuidv4 } from "uuid";
+import { EditTodoForm } from "./EditTodoForm";
+
+export const TodoWrapper = () => {
+  const [todos, setTodos] = useState(() => {
+    // Retrieve from localStorage if available
+    const savedTodos = localStorage.getItem("todos");
+    return savedTodos ? JSON.parse(savedTodos) : [];
+  });
+
+  useEffect(() => {
+    // Update localStorage whenever `todos` changes
+    localStorage.setItem("todos", JSON.stringify(todos));
+  }, [todos]);
+
+  const addTodo = (todo) => {
+    setTodos([
+      ...todos,
+      { id: uuidv4(), task: todo, completed: false, isEditing: false },
+    ]);
+  };
+
+  const deleteTodo = (id) => setTodos(todos.filter((todo) => todo.id !== id));
+
+  const toggleComplete = (id) => {
+    setTodos(
+      todos.map((todo) =>
+        todo.id === id ? { ...todo, completed: !todo.completed } : todo
+      )
+    );
+  };
+
+  const editTodo = (id) => {
+    setTodos(
+      todos.map((todo) =>
+        todo.id === id ? { ...todo, isEditing: !todo.isEditing } : todo
+      )
+    );
+  };
+
+  const editTask = (task, id) => {
+    setTodos(
+      todos.map((todo) =>
+        todo.id === id ? { ...todo, task, isEditing: !todo.isEditing } : todo
+      )
+    );
+  };
+
+  return (
+    <div className="TodoWrapper">
+      <h1>Get Things Done!</h1>
+      <TodoForm addTodo={addTodo} />
+      {/* display todos */}
+      {todos.map((todo) =>
+        todo.isEditing ? (
+          <EditTodoForm editTodo={editTask} task={todo} key={todo.id} />
+        ) : (
+          <Todo
+            key={todo.id}
+            task={todo}
+            deleteTodo={deleteTodo}
+            editTodo={editTodo}
+            toggleComplete={toggleComplete}
+          />
+        )
+      )}
+    </div>
+  );
+};
